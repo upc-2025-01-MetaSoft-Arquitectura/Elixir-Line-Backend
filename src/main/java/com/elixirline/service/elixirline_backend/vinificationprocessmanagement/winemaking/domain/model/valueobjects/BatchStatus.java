6ff98d9a15248ce23df7eb57a88196baf681876e
claudio moreno <u20191e800@upc.edu.pe>
2025-06-12 16:57:10 -0500

    fix(campaignmanagement): Files not corresponding to functionality are removed

--- src/main/java/com/elixirline/service/elixirline_backend/vinificationprocessmanagement/winemaking/domain/model/valueobjects/BatchStatus.java
+++
@@ -1,7 +0,0 @@
-package com.elixirline.service.elixirline_backend.vinificationprocessmanagement.winemaking.domain.model.valueobjects;
-
-public enum BatchStatus {
-    RECEIVED,
-    IN_PROGRESS,
-    COMPLETED
-}
