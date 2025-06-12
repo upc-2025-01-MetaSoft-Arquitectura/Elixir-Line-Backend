b26b46db73528393c8b3962d56ad2b2e358cfe10
claudio moreno <u20191e800@upc.edu.pe>
2025-06-12 16:57:10 -0500

    fix(campaignmanagement): Files not corresponding to functionality are removed

--- src/main/java/com/elixirline/service/elixirline_backend/vinificationprocessmanagement/winemaking/domain/model/valueobjects/ReceptionDate.java
+++
@@ -1,8 +0,0 @@
-package com.elixirline.service.elixirline_backend.vinificationprocessmanagement.winemaking.domain.model.valueobjects;
-
-import com.fasterxml.jackson.annotation.JsonValue;
-import jakarta.persistence.Embeddable;
-
-@Embeddable
-public record ReceptionDate() {
-}
