12187003eebffa7fffd18365b0cda9e6172da3f2
claudio moreno <u20191e800@upc.edu.pe>
2025-06-19 08:12:39 -0500

    feat(feature/develop): Agricultural inputs were added

---
+++ src/main/java/com/elixirline/service/elixirline_backend/agriculturalactivitiesmanagement/tasks/domain/model/valueobjetcs/UnitType.java
@@ -0,0 +1,8 @@
+package com.elixirline.service.elixirline_backend.agriculturalactivitiesmanagement.tasks.domain.model.valueobjetcs;
+
+public enum UnitType {
+    KG,
+    LITRO,
+    ML,
+    UNIDAD
+}
